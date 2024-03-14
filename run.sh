81fdd2c92c5cd91fa010a0aa327b3174a0608372
krasow <krasow@u.northwestern.edu>
2024-03-14 16:41:28 -0500

    new run.sh and outputs

--- run.sh
+++ run.sh
@@ -1,1 +1,30 @@
+#!/usr/bin/bash/env bash
 # run script for stuff
+
+do_run () {
+	echo $PWD;
+	echo "FLOAT TEST";
+	#make clean && make TYPE=FLOAT NR_DPUS=64
+	#./bin/host_code
+	echo "INT TEST";
+	make clean && make TYPE=INT32 NR_DPUS=64
+	./bin/host_code
+}
+
+cd prim-benchmarks
+prim_home=$PWD
+
+echo "_____________________________"
+echo "Running Figure 1"
+cd $prim_home/SCAN-RSS
+do_run
+cd $prim_home/SCAN-SSA
+do_run
+cd $prim_home/VA
+do_run
+cd $prim_home/RED
+do_run
+
+
+
+
