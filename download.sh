5ec63451f4a14a1a3386c4bdf83e0491797874ce
krasow <krasow@u.northwestern.edu>
2024-03-14 13:53:26 -0500

    first commit

---
+++ download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# pre req of Ubuntu 22.04 x86
+wget http://sdk-releases.upmem.com/2023.2.0/ubuntu_22.04/upmem-2023.2.0-Linux-x86_64.tar.gz
+tar -xvf upmem-2023.2.0-Linux-x86_64.tar.gz
+mv upmem-2023.2.0-Linux-x86_64 upmem
+cd upmem
+source upmem-env.sh
+
+git clone git@github.com:CMU-SAFARI/prim-benchmarks.git
+
