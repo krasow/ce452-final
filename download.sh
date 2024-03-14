1d9de857f111a0e5b7a56bf2432bc48e4cc61819
David Krasowska <krasowska89@gmail.com>
2024-03-14 14:00:41 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ wget http://sdk-releases.upmem.com/2023.2.0/ubuntu_22.04/upmem-2023.2.0-Linux-x8
 tar -xvf upmem-2023.2.0-Linux-x86_64.tar.gz
 mv upmem-2023.2.0-Linux-x86_64 upmem
 cd upmem
-source upmem-env.sh
+source upmem-env.sh simulator
 
 git clone git@github.com:CMU-SAFARI/prim-benchmarks.git
 
