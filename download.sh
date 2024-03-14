7c2cb06489921cd55aef00f1ed4f5f59d3691fa9
David <david@krasow-desk>
2024-03-14 14:51:51 -0500

    download me

--- download.sh
+++ download.sh
@@ -2,22 +2,24 @@
 
 # pre req of Ubuntu 22.04 x86
 
+pd=$PWD
+echo $pd
 
 # get anaconda locally
-mkdir -p ./miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ./miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ./miniconda3/miniconda.sh
-./miniconda3/bin/conda init bash
+mkdir -p $pd/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $pd/miniconda3/miniconda.sh
+bash $pd/miniconda3/miniconda.sh -b -u -p $pd/miniconda3
+rm -rf $pd/miniconda3/miniconda.sh
+$pd/miniconda3/bin/conda init bash
 conda env create -f environment.yml
 
 
 wget http://sdk-releases.upmem.com/2023.2.0/ubuntu_22.04/upmem-2023.2.0-Linux-x86_64.tar.gz
 tar -xvf upmem-2023.2.0-Linux-x86_64.tar.gz
 mv upmem-2023.2.0-Linux-x86_64 upmem
-cd upmem
+cd $pd/upmem
 source upmem_env.sh simulator
 
-cd ..
+cd $pd
 git clone git@github.com:CMU-SAFARI/prim-benchmarks.git
 
