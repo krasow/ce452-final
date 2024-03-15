9e8bda42dc9ba6d24cb653551a119855bda5cd7a
krasow <krasow@u.northwestern.edu>
2024-03-14 19:48:54 -0500

    figure 3 res

--- operator_test/dpu/task.c
+++ operator_test/dpu/task.c
@@ -1,78 +1,56 @@
-/*
-* Vector addition with multiple tasklets
-*
-*/
 #include <stdint.h>
 #include <stdio.h>
 #include <defs.h>
 #include <mram.h>
 #include <alloc.h>
-#include <perfcounter.h>
+#include <mram.h>
 #include <barrier.h>
+#include <perfcounter.h>
 
-#include "../support/common.h"
+__host uint32_t nb_perf;
 
-__host dpu_arguments_t DPU_INPUT_ARGUMENTS;
 
-// vector_addition: Computes the vector addition of a cached block 
-static void vector_addition(T *bufferB, T *bufferA, unsigned int l_size) {
-    for (unsigned int i = 0; i < l_size; i++){
-        bufferB[i] += bufferA[i];
-    }
-}
-
-// Barrier
 BARRIER_INIT(my_barrier, NR_TASKLETS);
 
-extern int main_kernel1(void);
+int main(void) {
+	uint16_t tasklet_id = me();
 
-int (*kernels[nr_kernels])(void) = {main_kernel1};
-
-int main(void) { 
-    // Kernel
-    return kernels[DPU_INPUT_ARGUMENTS.kernel](); 
-}
-
-// main_kernel1
-int main_kernel1() {
-    unsigned int tasklet_id = me();
-#if PRINT
-    printf("tasklet_id = %u\n", tasklet_id);
-#endif
-    if (tasklet_id == 0){ // Initialize once the cycle counter
+    if (tasklet_id == 0) { // Initialize once the cycle counter
         mem_reset(); // Reset the heap
     }
-    // Barrier
-    barrier_wait(&my_barrier);
+	
+	uint32_t mram_base_addr     = (uint32_t) DPU_MRAM_HEAP_POINTER;	
+	
+#ifdef FLOAT
+	volatile float a = 3.231;
+	volatile float b = 13.495;
+	volatile float res;
+#else 
+    volatile int a = 3;
+	volatile int b = 13;
+	volatile int res;
+#endif
 
-    uint32_t input_size_dpu_bytes = DPU_INPUT_ARGUMENTS.size; // Input size per DPU in bytes
-    uint32_t input_size_dpu_bytes_transfer = DPU_INPUT_ARGUMENTS.transfer_size; // Transfer input size per DPU in bytes
+	perfcounter_config(COUNT_INSTRUCTIONS, true);
+#ifdef ADDITION
+	res = a + b;
+#endif
+#ifdef SUBTRACTION
+	res = a - b;
+#endif
+#ifdef MULT
+	res = a * b;
+#endif
+#ifdef DIV
+	res = a / b;
+#endif
+	nb_perf = perfcounter_get();
+	
 
-    // Address of the current processing block in MRAM
-    uint32_t base_tasklet = tasklet_id << BLOCK_SIZE_LOG2;
-    uint32_t mram_base_addr_A = (uint32_t)DPU_MRAM_HEAP_POINTER;
-    uint32_t mram_base_addr_B = (uint32_t)(DPU_MRAM_HEAP_POINTER + input_size_dpu_bytes_transfer);
-
-    // Initialize a local cache to store the MRAM block
-    T *cache_A = (T *) mem_alloc(BLOCK_SIZE);
-    T *cache_B = (T *) mem_alloc(BLOCK_SIZE);
-
-    for(unsigned int byte_index = base_tasklet; byte_index < input_size_dpu_bytes; byte_index += BLOCK_SIZE * NR_TASKLETS){
-
-        // Bound checking
-        uint32_t l_size_bytes = (byte_index + BLOCK_SIZE >= input_size_dpu_bytes) ? (input_size_dpu_bytes - byte_index) : BLOCK_SIZE;
-
-        // Load cache with current MRAM block
-        mram_read((__mram_ptr void const*)(mram_base_addr_A + byte_index), cache_A, l_size_bytes);
-        mram_read((__mram_ptr void const*)(mram_base_addr_B + byte_index), cache_B, l_size_bytes);
-
-        // Computer vector addition
-        vector_addition(cache_B, cache_A, l_size_bytes >> DIV);
-
-        // Write cache to current MRAM block
-        mram_write(cache_B, (__mram_ptr void*)(mram_base_addr_B + byte_index), l_size_bytes);
-
-    }
-
-    return 0;
+	if (tasklet_id == 0){
+		mram_write((void *)&res, (__mram_ptr void*)mram_base_addr, 8);
+	}
+	
+	barrier_wait(&my_barrier);
+	return 0;
 }
--- scripts/run3.sh
+++ scripts/run3.sh
@@ -6,11 +6,23 @@ do_run () {
 	echo $PWD;
 	echo "";
 	echo "FLOAT TEST";
-	make clean && make TYPE=FLOAT NR_DPUS=64
+	make clean && make TYPE=FLOAT TEST=ADDITION NR_DPUS=1
+	./bin/host_code
+	make clean && make TYPE=FLOAT TEST=SUBTRACTION NR_DPUS=1
+	./bin/host_code
+	make clean && make TYPE=FLOAT TEST=MULT NR_DPUS=1
+	./bin/host_code
+	make clean && make TYPE=FLOAT TEST=DIV NR_DPUS=1
 	./bin/host_code
 	echo "";
 	echo "INT TEST";
-	make clean && make TYPE=INT32 NR_DPUS=64
+	make clean && make TYPE=INT32 TEST=ADDITION NR_DPUS=1
+	./bin/host_code
+	make clean && make TYPE=INT32 TEST=SUBTRACTION NR_DPUS=1
+	./bin/host_code
+	make clean && make TYPE=INT32 TEST=MULT NR_DPUS=1
+	./bin/host_code
+	make clean && make TYPE=INT32 TEST=DIV NR_DPUS=1
 	./bin/host_code
 }
 
